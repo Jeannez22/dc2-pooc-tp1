75e68cd83c19439ac71a6389062b27772a3cb084
Jeannez22 <jeanne.noel_metayer@yahoo.com>
2018-09-27 09:56:44 +0000

    Ajout TP LED

---
+++ server-led/serveur.js
@@ -0,0 +1,41 @@
+const express = require('express')
+  const app = express()
+  const port = 3000
+//OS est un utilitaire node qui va nous servir à afficher le nom de notre raspberry
+const os = require("os");
+//MustacheExpress est notre moteur de template
+const mustacheExpress = require('mustache-express');
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+//Ici on dit au serveur de servir les fichiers statiques depuis le dossier /public
+app.use(express.static('public'))
+
+  app.get('/on', (request, response) => {
+    response.render('on')
+})
+
+app.get('/off', (request, response) => {
+	response.render('off')
+})
+
+  app.get('/hello/:name', (request, response) => {
+    response.render('hello', {name: request.params.name});
+})
+
+app.get('/pooc', (request, response) => {
+  //Ici on indique que nous voulons transformer notre fichier index.mustache en HTML
+  response.render('pooc');
+})
+
+  app.listen(port, (err) => {
+    if (err) {
+      return console.log('Erreur du serveur : ', err)
+    }
+    //On utilise l'utilitaire OS pour récupérer le nom de notre raspberry.
+  console.log('Le serveur écoute sur le port '+port+'\nRendez vous sur http://'+os.hostname()+'.local:'+port);
+})
+
+process.on('SIGINT', () => {
+  led.unexport();
+});
