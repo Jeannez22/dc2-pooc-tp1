391cdf4b0d4bfbeb613c749bb204f9f70afe6c87
Jeannez22 <jeanne.noel_metayer@yahoo.com>
2018-09-27 08:58:40 +0000

    Ajout fichier TP2

--- serveur.js
+++ serveur.js
@@ -1,15 +1,32 @@
 const express = require('express')
   const app = express()
   const port = 3000
+//OS est un utilitaire node qui va nous servir à afficher le nom de notre raspberry
+const os = require("os");
+//MustacheExpress est notre moteur de template
+const mustacheExpress = require('mustache-express');
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+//Ici on dit au serveur de servir les fichiers statiques depuis le dossier /public
+app.use(express.static('public'))
+
   app.get('/', (request, response) => {
-    response.send('Requête reçue...')
+    response.render('index')
 })
   app.get('/hello/:name', (request, response) => {
-    response.send('Hello, '+request.params.name+'!');
+    response.render('hello', {name: request.params.name});
 })
+
+app.get('/pooc', (request, response) => {
+  //Ici on indique que nous voulons transformer notre fichier index.mustache en HTML
+  response.render('pooc');
+})
+
   app.listen(port, (err) => {
     if (err) {
       return console.log('Erreur du serveur : ', err)
     }
-    console.log('Le serveur écoute sur le port '+port+'Rendez vous sur http://localhost:'+port);
+    //On utilise l'utilitaire OS pour récupérer le nom de notre raspberry.
+  console.log('Le serveur écoute sur le port '+port+'\nRendez vous sur http://'+os.hostname()+'.local:'+port);
 })
