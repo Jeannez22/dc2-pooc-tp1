87c6329b5a50ac71fc5328404e1b67c8e3ef5b81
Jeannez22 <jeanne.noel_metayer@yahoo.com>
2018-10-11 09:49:07 +0000

    TP2 part 3 + buzzer

--- server-led/serveur.js
+++ server-led/serveur.js
@@ -3,21 +3,60 @@ const express = require('express')
   const port = 3000
 //OS est un utilitaire node qui va nous servir à afficher le nom de notre raspberry
 const os = require("os");
+
+// Déclaration des constantes GPIO  sleep  led 1 et led 2
+const Gpio = require('onoff').Gpio;
+const sleep = require('sleep');
+const led1 = new Gpio(4, 'out');
+const led2 = new Gpio(15, 'out');
+const buzz = new Gpio(21,'out');
+
 //MustacheExpress est notre moteur de template
 const mustacheExpress = require('mustache-express');
 app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
+
 //Ici on dit au serveur de servir les fichiers statiques depuis le dossier /public
-app.use(express.static('public'))
+app.use(express.static('views'))
 
-  app.get('/on', (request, response) => {
-    response.render('on')
+// Allumer la led 1
+  app.get('/on/1', (request, response) => {
+ led1.writeSync(1);
+  response.render('on');
 })
 
-app.get('/off', (request, response) => {
-	response.render('off')
+// Allumer la led 2
+
+app.get('/on/2', (request, response) => {
+led2.writeSync(1);
+response.render('on');
 })
+
+//Eteindre la led 1
+
+app.get('/off/1', (request, response) => {
+led1.writeSync(0);	
+ response.render('off');
+} )
+
+// Eteindre la led 2
+app.get('/off/2', (request, response) => {      
+led2.writeSync(0);
+ response.render('off');
+} )
+
+//Buzzer
+
+app.get('/buzz/on', (request, response) => {      
+buzz.writeSync(1);
+ response.render('on');
+} )
+
+app.get('/buzz/off', (request, response) => {      
+buzz.writeSync(0);
+ response.render('off');
+} )
 
   app.get('/hello/:name', (request, response) => {
     response.render('hello', {name: request.params.name});
@@ -27,7 +66,6 @@ app.get('/pooc', (request, response) => {
   //Ici on indique que nous voulons transformer notre fichier index.mustache en HTML
   response.render('pooc');
 })
-
   app.listen(port, (err) => {
     if (err) {
       return console.log('Erreur du serveur : ', err)
@@ -37,5 +75,5 @@ app.get('/pooc', (request, response) => {
 })
 
 process.on('SIGINT', () => {
-  led.unexport();
+  led1.unexport();
 });
